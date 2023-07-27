4c6044c7f6023808fbfdd2853c18d0591a7a2d3f
Diego Mazariegos <maz18808@uvg.edu.gt>
2023-07-27 14:04:42 -0600

    Inicializacion del reconocimiento de voz

--- Asistente_De_Voz.py
+++ Asistente_De_Voz.py
@@ -1,5 +1,9 @@
 # Aqui importe el primer pip que instale
-import openai       
+import openai
+
+# Se realizan nuevos imports para reconocimiento de voz
+import speech_recognition as sr
+import pyttsx3                      # py text to speech       
 
 # Despues vamos a inicializar ChatGPT
 # Para ello se necesita de una llave unica que genera ChatGPT
@@ -17,26 +21,34 @@ mensajes = [
     {"role": "system", "content": f"{mode}"}
 ]
 
+# Inicializamos el engine de text to speech
+engine = pyttsex3.init()
+voces = engine.getProperty('voces')      # Obtener todas la voces que tenemos instaladas en windows
+engine.setProperty('voice', voces[1].id) # 0 para hombre y 1 para mujer
+
+            
+
 # Lo que se hace en esta parte es tomar la entrada del usuario
 # mandarsela al API de OpenAI y despues leer la respuesta
+# El while se agrega para que siga preguntando
 
-# Creamos la entrada del usuario
-entrada_usuario = input('Que pregunta deseas hacer: ')
+while True:
+    # Creamos la entrada del usuario
+    entrada_usuario = input('Que pregunta deseas hacer: ')
 
-## Adjuntamos la lista
-mensajes.append({"role": "user": "content": entrada_uuario})
+    ## Adjuntamos la lista
+    mensajes.append({"role": "user": "content": entrada_uuario})
 
-# Hasta donde se gtp 3.5 turbo es como el motor que usar ChatGPT para funcionar
-# La variable temperature es el porcentaje de aleatoriedad de la respuesta
+    # Hasta donde se gtp 3.5 turbo es como el motor que usar ChatGPT para funcionar
+    # La variable temperature es el porcentaje de aleatoriedad de la respuesta
 
-afinacion = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo-0301",   # Estos son parametros
-            mensajes = mensajes             # que van a servirle
-            temperature = 0.8               # a la afinacion
-            )
+    afinacion = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo-0301",   # Estos son parametros
+                mensajes = mensajes             # que van a servirle
+                temperature = 0.8               # a la afinacion
+                )
 
-
-# Se imprime la respuesta
-respuesta = afinacion.choices[0].message.content                 # El cero es para la respuesta mas reciente
-mensajes.append({"role": "assistant", "content": respuesta})   # Se adjunta la lista de mensajes, se alamacena la conversacion
-print(f"\n(respuesta)\n")
+    # Se imprime la respuesta
+    respuesta = afinacion.choices[0].message.content                 # El cero es para la respuesta mas reciente
+    mensajes.append({"role": "assistant", "content": respuesta})   # Se adjunta la lista de mensajes, se alamacena la conversacion
+    print(f"\n(respuesta)\n")
